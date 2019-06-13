d395ed87260e2d9d9a69349c330de534dda20157
DeVyacheslav <deviacheslav@gmail.com>
2019-06-13 19:48:27 +0300

    GeneralCPU. Avg Steps: 64.3

--- mlis/problems/generalCpu.py
+++ mlis/problems/generalCpu.py
@@ -8,15 +8,147 @@ import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
 from ..utils import solutionmanager as sm
 from ..utils import gridsearch as gs
 
+class SolutionModel(nn.Module):
+    def __init__(self, input_size, output_size, solution):
+        super(SolutionModel, self).__init__()
+        self.input_size = input_size
+        sm.SolutionManager.print_hint("Hint[1]: Increase hidden size")
+        self.hidden_size = solution.hidden_size
+        self.linear1 = nn.Linear(input_size, self.hidden_size)
+        self.linear2 = nn.Linear(self.hidden_size, output_size)
+
+    
+    def forward(self, x):
+        x = nn.Sequential(self.linear1,
+                      nn.LeakyReLU(),
+                      self.linear2,
+                      nn.Sigmoid()
+            ).forward(x)
+        return x
+
+    def calc_error(self, output, target):
+        # This is loss function
+        result = (-1.0 * (target * torch.log(output) + (1.0 - target) * torch.log(1.0 - output))).sum()
+        # result = ((output-target)**2).sum()
+        return  result
+
+    def calc_predict(self, output):
+        # Simple round output to predict value
+        return output.round()
+
 class Solution():
+    def __init__(self):
+        # Control speed of learning
+        self.learning_rate = 0.043
+        self.weight_decay = 0.01
+        self.momentum = 0.1
+        # Control number of hidden neurons
+        self.hidden_size = 95
+        
+
+        # Grid search settings, see grid_search_tutorial
+        self.learning_rate_grid = [0.01, 0.001, 0.0001]
+        self.hidden_size_grid = [20, 24, 28, 30]
+        # grid search will initialize this field
+        self.grid_search = None
+        # grid search will initialize this field
+        self.iter = 0
+        # This fields indicate how many times to run with same arguments
+        self.iter_number = 2
+
     # Return trained model
     def train_model(self, train_data, train_target, context):
-        print("See helloXor for solution template")
-        exit(0)
+        def init_normal(m):
+            if type(m) == nn.Linear:
+                nn.init.uniform_(m.weight)
 
+        def init_weights(m):
+            if type(m) == nn.Linear:
+                torch.nn.init.xavier_uniform_(m.weight)
+                m.bias.data.fill_(0.01)
+        def weights_init_uniform_rule(m):
+            classname = m.__class__.__name__
+            # for every Linear layer in a model..
+            if classname.find('Linear') != -1:
+                # get the number of the inputs
+                n = m.in_features
+                y = 1.0/np.sqrt(n)
+
+                m.weight.data.uniform_(-y, y)
+                m.bias.data.fill_(0.001)
+
+        # Uncommend next line to understand grid search
+        # return self.grid_search_tutorial()
+        # Model represent our neural network
+
+        model = SolutionModel(train_data.size(1), train_target.size(1), self)
+        model.apply(weights_init_uniform_rule)
+
+        # Optimizer used for training neural network
+        sm.SolutionManager.print_hint("Hint[2]: Learning rate is too small", context.step)
+        optimizer = optim.Adagrad(model.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+        # model.parameters()...gradient set to zero
+        # optimizer.zero_grad()
+        while True:
+            # Report step, so we know how many steps
+            context.increase_step()
+            # model.parameters()...gradient set to zero
+            # optimizer.zero_grad()
+
+            # evaluate model => model.forward(data)
+
+            temp = torch.cat((train_data, train_target), 1)
+            temp = temp[torch.randperm(temp.size()[0])]
+
+            train_data = temp[:, :-1]
+            train_target = temp[:, -1:]
+
+            output = model(train_data)
+            
+            # if x < 0.5 predict 0 else predict 1
+            predict = model.calc_predict(output)
+            # Number of correct predictions
+            correct = predict.eq(train_target.view_as(predict)).long().sum().item()
+            # Total number of needed predictions
+            total = predict.view(-1).size(0)
+            # No more time left or learned everything, stop training
+            time_left = context.get_timer().get_time_left()
+            if time_left < 0.1 or correct == total:
+                break
+            # calculate error
+            error = model.calc_error(output, train_target)
+            # calculate deriviative of model.forward() and put it in model.parameters()...gradient
+            error.backward()
+            # print progress of the learning
+            self.print_stats(context.step, error, correct, total)
+            # update model: model.parameters() -= lr * gradient
+            optimizer.step()
+
+        return model
+
+    def print_stats(self, step, error, correct, total):
+        if step % 1000 == 0:
+            print("Step = {} Correct = {}/{} Error = {}".format(step, correct, total, error.item()))
+    
+    def grid_search_tutorial(self):
+        # During grid search every possible combination in field_grid, train_model will be called
+        # iter_number times. This can be used for automatic parameters tunning.
+        if self.grid_search:
+            print("[HelloXor] learning_rate={} iter={}".format(self.learning_rate, self.iter))
+            self.grid_search.add_result('iter', self.iter)
+            if self.iter == self.iter_number-1:
+                print("[HelloXor] chose_str={}".format(self.grid_search.choice_str))
+                print("[HelloXor] iters={}".format(self.grid_search.get_results('iter')))
+                stats = self.grid_search.get_stats('iter')
+                print("[HelloXor] Mean={} Std={}".format(stats[0], stats[1]))
+        else:
+            print("Enable grid search: See run_grid_search in the end of file")
+            exit(0)
 ###
 ###
 ### Don't change code after this line
@@ -62,7 +194,7 @@ class Config:
 
 run_grid_search = False
 # Uncomment next line if you want to run grid search
-#run_grid_search = True
+# run_grid_search = True
 if run_grid_search:
     gs.GridSearch().run(Config(), case_number=1, random_order=False, verbose=False)
 else:
